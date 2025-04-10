285b2d31441803c0c03854e9d9018bdcf5ee4090
Rob Colquhoun <robcolquhoun2020@Robs-MacBook-Pro.local>
2025-04-09 20:50:44 -0400

    redis ig

--- config/database.php
+++ config/database.php
@@ -149,7 +149,7 @@ return [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
-            'serializer' => Redis::SERIALIZER_IGBINARY
+            'serializer' => 'igbinary'
         ],
 
         'default' => [
