63a87035563d6f5b9851bc1ef55c74f857256340
Rob Colquhoun <robcolquhoun2020@Robs-MacBook-Pro.local>
2025-09-03 19:39:31 -0400

    typo

--- app/Listeners/HandleFantraxUserConnected.php
+++ app/Listeners/HandleFantraxUserConnected.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
 use App\Events\BotFantraxLinked;
-
+use App\Models\SocialAccount;
 
 /**
  * Listener that runs after a user connects Fantrax.
