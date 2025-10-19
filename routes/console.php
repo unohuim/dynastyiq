23a1fec13fedf85208e92e586e0f977275b023ca
Rob Colquhoun <robcolquhoun2020@Robs-MacBook-Pro.local>
2025-10-19 14:18:23 -0400

    scheduler

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 
-// Schedule::command('nhl:discover --days=7')->dailyAt('03:30');
-// Schedule::command('nhl:process --limit=500')->everyTenMinutes();
+Schedule::command('nhl:discover --days=2')->dailyAt('03:50');
+Schedule::command('nhl:process --limit=500')->everyTenMinutes();
