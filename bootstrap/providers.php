63de999ff05ace892f53b394d155af97072db3d1
Rob Colquhoun <robc@Robs-MacBook-Pro-2.local>
2026-07-01 21:54:19 -0400

    horizonserviceprovider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,6 @@ return [
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     SocialiteProviders\Manager\ServiceProvider::class,
 ];
